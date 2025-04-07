44278d683eaab0d8d780b18d0d5ab2890896454e
UbongPr7 <ubongpr7@gmail.com>
2025-04-07 16:50:19 +0100

    I took mayor through list methods

--- class_2/main.py
+++ class_2/main.py
@@ -9,8 +9,10 @@ shopping_cart =[
 98.99,
 "shoes",
 10,
-"a covered shoe , versace product, "
+"a cover shoe , versace product, "
 "heel",
+True,
+True,
 True,
 3
 ]
@@ -20,3 +22,42 @@ print(shopping_cart[-1])
 print(shopping_cart[-2])
 
 #list methods
+# method is a function that belongs to an object
+# function is a block of code that can be called many times to perform a particular task
+
+#1 append is used to add item to the end of a list
+shopping_cart.append(2)
+item_to_be_in_last_pos='Hurry to buy now'
+shopping_cart.append(item_to_be_in_last_pos)
+print(shopping_cart)
+
+#2 insert is used to add item to a list at a specified position
+position = 3
+data_to_insert ="15 pieces remaining"
+shopping_cart.insert(position,data_to_insert)
+
+print(shopping_cart)
+
+#3 remove is used to remove an item from a list
+item_to_remove =True
+shopping_cart.remove(item_to_remove)
+print(shopping_cart)
+
+# 4 pop is used to remove an item from a list and return the item
+item_to_pop = shopping_cart.pop()
+print(item_to_pop)
+print(shopping_cart.pop())
+
+print(shopping_cart)
+
+
+
+# 4 sort is used to sort a list in ascending order, is used for int and float
+shopping_cart.sort()
+print(shopping_cart)
+
+
+
+
+
+
