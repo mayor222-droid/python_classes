775b406eda12b96ea896ba618fd70047b9841edc
Mayor <mayokunadetiba@gmail.com>
2025-04-07 16:23:54 -0700

    practical class on creating a list

---
+++ class_2/main.py
@@ -0,0 +1,22 @@
+friends =["taye", "kehinde","idowu"]
+print(friends)
+print(friends[1])
+print(len(friends))
+friends[2]="omowumi"
+print(friends)
+
+shopping_cart =[
+98.99,
+"shoes",
+10,
+"a covered shoe , versace product, "
+"heel",
+True,
+3
+]
+print(shopping_cart[3])
+#how to print the last item
+print(shopping_cart[-1])
+print(shopping_cart[-2])
+
+#list methods
