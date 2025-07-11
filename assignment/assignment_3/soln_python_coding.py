ec37959df02d5e30d663c0b1026b71d5d76371da
Mayor <mayokunadetiba@gmail.com>
2025-07-11 20:02:10 +0100

    soln to python coding

--- assignment/assignment_3/soln_python_coding.py
+++ assignment/assignment_3/soln_python_coding.py
@@ -9,3 +9,20 @@ print(friuts.count("apple"))
 #dictionary with product details
 product= {"name": "clothing", "categories": "fashion", "price":100, "quantity":20, "SKU":"MA011YOR", "availability":"true","manufacturer":"dior" }
 print(product)
+
+print(product)
+print(product['name'])
+print(product.get('price'))
+print(product.get('quantity'))
+
+description=f"the products are {{name}}, by {{manufacturer}}, cost ${{price}}, and has {{stock_quantity}}, units available"
+print(description)
+
+# sales_order
+# customer_clothing
+# product_dior
+# unit_100
+# total_100
+print(product)
+
+
