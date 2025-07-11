1f192129e7aa92215410bf2b64a9287686f4d21f
Mayor <mayokunadetiba@gmail.com>
2025-07-11 19:20:44 +0100

    soln to python coding

---
+++ assignment/assignment_4/soln_to_python_coding_2.py
@@ -0,0 +1,84 @@
+animals=("monkey","dog","cat","cow","pig","elephant","sheep","monkey","pig")
+print(animals)
+print(animals[-1])
+print(animals.count("monkey"))
+print(animals.index("elephant"))
+
+#dictionary practice
+student= {"name": "precious", "age": 19, "school": "Visa", "grade": "A", "nationality": "Nigerian",}
+print(student)
+
+#keys()
+
+print(f'values are:{student.values()}')
+print(f'items are:{student.items()}')
+
+#get
+(student.get("hobby"))
+print("no hobby recorded")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#set operations and logic
+my_set={1,2,3,4,5,6,7,8,9,10,6,6,4,4}
+print(my_set)
+my_set.add(35)
+print(my_set)
+my_set.update(['cloth','bag',23.5,24.5])
+print(my_set)
+
+
+# set operation
+even_numbers={2,4,6,8,10,12,14,16,18,20}
+odd_numbers={1,3,5,7,11,13,15,17,19,23}
+# print(even_numbers.union(odd_numbers))
+# print(even_numbers.intersection(my_set))
+# print(even_numbers.difference(odd_numbers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
